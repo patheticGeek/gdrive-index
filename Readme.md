55d937ab81be20aaf98ec03bcba7c8c039c8085b
Pathetic Geek <geekpathetic@gmail.com>
2020-06-19 23:18:26 +0530

    init

---
+++ Readme.md
@@ -0,0 +1,4 @@
+# Gdrive index
+
+This lets you deploy a index server on heroku which serves files from your gdrive. It supports both gdrive and teamdrive.
+As teamdrive links cannot be share with people out of the drive this will let you do it and this dosent have a limit on downloads from drive.
---
+++ index.js
@@ -0,0 +1,19 @@
+const compression = require("compression");
+const express = require("express");
+const dev = process.env.NODE_ENV !== "production";
+const PORT = parseInt(process.env.PORT, 10) || 3000;
+
+console.log(`Dev: ${dev}`);
+
+const server = express();
+
+server.use(compression());
+server.use((req, res, next) => {
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader("Access-Control-Allow-Headers", "*");
+  next();
+});
+
+server.get("/ping", (req, res) => res.send("pong"));
+
+server.listen(PORT, () => console.log(`> Listining on http://localhost:${PORT}`));

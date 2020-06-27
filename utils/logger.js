d2fe80ca2433a3819cde67f0deb2973c9fe76aef
Pathetic Geek <geekpathetic@gmail.com>
2020-06-28 01:48:56 +0530

    logger util added

---
+++ utils/logger.js
@@ -0,0 +1,25 @@
+const fs = require("fs");
+
+const logFile = "./logs.txt";
+
+fs.writeFileSync(logFile, "");
+
+function logger(...args) {
+  args.forEach((arg) => {
+    if (typeof arg === "string" || typeof arg === "number") {
+      toWrite += arg;
+    } else {
+      try {
+        const stringified = JSON.stringify(arg);
+        toWrite += stringified;
+      } catch (e) {
+        toWrite += arg;
+      }
+    }
+    toWrite += " ";
+  });
+  console.log(toWrite);
+  fs.appendFileSync(logFile, "\n" + toWrite);
+}
+
+module.exports = logger;
